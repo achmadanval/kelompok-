3baa766804a4031a579a18d09e917dc5b988739a
achmadanval <anvalalan@gmail.com>
2024-09-04 11:54:51 +0700

    Update Latihan021.java

--- Latihan021.java
+++ Latihan021.java
@@ -29,3 +29,23 @@ public class TugasDaspro1{
         System.out.print("Nama     :Rachmad Aprisandhy \nNIM      :244107020148 \nKelas    :TI1B \nNo Absen :19 \nProdi    :Teknik Informatika \nAlamat   :Blitar \nNo Hp    :085233540866 \nHobi     :Memancing");
     }
 }
+/**
+ * Tugas3
+ */
+public class Tugas3 {
+
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama    :Bagas Ardiansa Putra");
+        System.out.println("NIM     :244107020166");
+        System.out.println("Kelas   :1B");
+        System.out.println("No Absen:06");
+        System.out.println("Prodi   :Teknik Informatika");
+        System.out.println("Alamat  :Perum Taman Anggrek Saptorenggo Pakis");
+        System.out.println("No HP   :085233647130");
+        System.out.println("Hobi    :Game");
+    }
+    
+}
